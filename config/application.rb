bfc6b51c2e7b74e97f2d26cd5e2c9a02859cfeab
ignacio-sp <ignaciosanhueza233@uc.cl>
2024-05-09 01:43:43 -0400

    testing bootstrap template

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,17 @@ module Terrasense
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.assets.paths << Rails.root.join("app/assets", 'images')
+    
+    config.assets.paths << Rails.root.join("app/assets", 'blog')
+    
+    config.assets.paths << Rails.root.join("app/assets", 'clients')
+    
+    config.assets.paths << Rails.root.join("app/assets", 'portfolio')
+    
+    config.assets.paths << Rails.root.join("app/assets", 'team')
+    
+    config.assets.paths << Rails.root.join("app/assets", 'testimonials')
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
